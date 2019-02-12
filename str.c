84d889ce450ffd00aab303810287c1bdf6976d7c
arun2431 <47448278+arun2431@users.noreply.github.com>
2019-02-13 02:03:33 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+  
+  int i,j;
+  char s[100];
+ 
+  scanf("%s %d",&s,&i);
+   
+  for(j=0;j<i;j++)
+  printf("%s\n",s);
+  
+  return 0;
+
+
+}
