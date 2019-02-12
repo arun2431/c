8b0bae176ce847547e4053c3187023491bb23c82
arun2431 <47448278+arun2431@users.noreply.github.com>
2019-02-13 00:33:30 +0530

    Update and rename ev.c to eve.c

--- eve.c
+++ eve.c
@@ -3,15 +3,17 @@
 int main()
 {
   
-  int n,j,r;
+  int n,j,r,a[100],i=0;
   scanf("%d%d",&n,&r);
   for(j=n+1;j<r;j++)
   {
       if(j%2== 0)
       {
-          printf("%d\t",j);
+         a[i]=j;
+         i++;
       }
   }
-  
+  for(j=0;j<i;j++)
+  printf("%d\t",a[j]);
  return 0;
 }
